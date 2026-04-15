f2cad5410a617e2e4b9b3935c3347c8c8f57593d
adminssaha <105242694+AdminSSAHA@users.noreply.github.com>
2026-04-15 11:31:42 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -10,8 +10,4 @@ terraform {
 
 provider "azurerm" {
   features {}
-  client_id       = "434dcd82-9505-4e4d-adfb-ec0fe56edc36"
-  client_secret   = "sNC8Q~jsfVCOrmb2ID-e0PtT3Zl_ggt4DY.Ipdcr"
-  tenant_id       = "50cd50eb-068d-4db6-b072-2b3290ff2fe6"
-  subscription_id = "b5f22164-a376-43b3-96c1-58d4aa9fb7a6"
 }
