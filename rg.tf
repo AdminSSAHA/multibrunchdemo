fe8afb1dc33798711d86d03927f5077cc5d9f98d
adminssaha <105242694+AdminSSAHA@users.noreply.github.com>
2026-04-15 14:32:43 +0530

    Update rg.tf

--- rg.tf
+++ rg.tf
@@ -1,8 +1,8 @@
-resource "azurerm_resource_group" "resource_rg" {
-  name     = "rg-ss-tfcloud-1"
+resource "azurerm_resource_group" "resource_rg_main" {
+  name     = "rg-ss-tfcloud-main-1"
   location = "eastus"
 }
 
-output "rg_name" {
+output "rg_name_main" {
   value = azurerm_resource_group.resource_rg
 }
