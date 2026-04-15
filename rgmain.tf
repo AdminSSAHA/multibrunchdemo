3f173e0b34a8b43b698fcf32bd572a4c4e36de65
adminssaha <105242694+AdminSSAHA@users.noreply.github.com>
2026-04-15 14:43:48 +0530

    Create rgmain.tf

---
+++ rgmain.tf
@@ -0,0 +1,8 @@
+resource "azurerm_resource_group" "resource_rg_main" {
+  name     = "rg-ss-tfcloud-main-1"
+  location = "eastus"
+}
+
+output "rg_name_main" {
+  value = azurerm_resource_group.resource_rg_main
+}
