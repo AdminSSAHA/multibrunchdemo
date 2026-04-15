527d2a608764a69015152652d4be0c05bb53c7e7
adminssaha <105242694+AdminSSAHA@users.noreply.github.com>
2026-04-15 12:41:20 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,23 @@
+variable "subscription_id" {
+ description = "Azure Subscription ID"
+ type        = string
+ sensitive   = true
+}
+ 
+variable "tenant_id" {
+ description = "Azure Tenant ID"
+ type        = string
+ sensitive   = true
+}
+ 
+variable "client_id" {
+ description = "Service Principal Client ID (App ID)"
+ type        = string
+ sensitive   = true
+}
+ 
+variable "client_secret" {
+ description = "Service Principal Client Secret"
+ type        = string
+ sensitive   = true
+}
